c829cc81e73d85aa1d334e3859532b7e9d459780
Roger Luethi <rl@patchworkscience.org>
2018-06-25 15:17:18 +0200

    fix

--- recipes/galera.rb
+++ recipes/galera.rb
@@ -195,7 +195,7 @@ iface = if node['mariadb']['galera']['wsrep_node_address_interface'].empty?
           node['mariadb']['galera']['wsrep_node_address_interface']
         end
 puts "RLX ifaces:#{node['network']['interfaces']}"
-ifcace = 'lo'
+iface = 'lo'
 puts "RLX iface:#{iface}"
 node['network']['interfaces'][iface]['addresses'].each do |ip, params|
   if params['family'] == 'inet'
